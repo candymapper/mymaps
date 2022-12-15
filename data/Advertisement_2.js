a63070f2e7f885f8eef341eee94965046064bdb9
candymapper <usmananwar77555@gmail.com>
2022-12-15 19:24:10 +0500

    Weebmaps

---
+++ data/Advertisement_2.js
@@ -0,0 +1,8 @@
+var json_Advertisement_2 = {
+"type": "FeatureCollection",
+"name": "Advertisement_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Click_Me": "2" }, "geometry": { "type": "Point", "coordinates": [ 73.173413638088348, 33.610964850037064 ] } }
+]
+}
